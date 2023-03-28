f2e78a02f2bc7a4d1ace7e84e1cf48336e616562
Taehwa Kim <kth1149@gmail.com>
2023-03-28 23:36:33 +0900

    230328

--- dl_bootcamp_pytorch/chapter15_project_practice_1/train.py
+++ dl_bootcamp_pytorch/chapter15_project_practice_1/train.py
@@ -35,27 +35,21 @@ def define_argparser():
 
 
 def main(config):
-    # Set device based on user defined configuration.
-    device = torch.device('cpu') if config.gpu_id < 0 else torch.device('cuda:%d' % config.gpu_id)
+    device = torch.device('cpu') if config.gpu_id < 0 else torch.device(f'cuda:{config.gpu_id}')
 
-    x, y = load_mnist(is_train=True, flatten=True)
-    x, y = split_data(x.to(device), y.to(device), train_ratio=config.train_ratio)
+    X, y = load_mnist(is_train=True, flatten=True)
+    X, y = split_data(X.to(device), y.to(device), train_ratio=config.train_ratio)
 
-    print("Train:", x[0].shape, y[0].shape)
-    print("Valid:", x[1].shape, y[1].shape)
+    print("Train:", X[0].shape, y[0].shape)
+    print("Valid:", X[1].shape, y[1].shape)
 
-    input_size = int(x[0].shape[-1])
+    input_size = int(X[0].size(-1))
     output_size = int(max(y[0])) + 1
 
-    model = ImageClassifier(
-        input_size=input_size,
-        output_size=output_size,
-        hidden_sizes=get_hidden_sizes(input_size,
-                                      output_size,
-                                      config.n_layers),
-        use_batch_norm=not config.use_dropout,
-        dropout_p=config.dropout_p,
-    ).to(device)
+    model = ImageClassifier(input_size=input_size, output_size=output_size,
+                            hidden_sizes=get_hidden_sizes(input_size, output_size, config.n_layers),
+                            use_batch_norm=not config.use_dropout,
+                            dropout_p=config.dropout_p).to(device)
     optimizer = optim.Adam(model.parameters())
     crit = nn.NLLLoss()
 
@@ -66,17 +60,11 @@ def main(config):
 
     trainer = Trainer(model, optimizer, crit)
 
-    trainer.train(
-        train_data=(x[0], y[0]),
-        valid_data=(x[1], y[1]),
-        config=config
-    )
+    trainer.train(train_data=(X[0], y[0]), valid_data=(X[1], y[1]), config=config)
 
-    # Save best model weights.
-    torch.save({
-        'model': trainer.model.state_dict(),
-        'opt': optimizer.state_dict(),
-        'config': config,
+    torch.save({'model': trainer.model.state_dict(),
+                'opt': optimizer.state_dict(),
+                'config': config,
     }, config.model_fn)
 
 
--- dl_bootcamp_pytorch/chapter15_project_practice_1/trainer.py
+++ dl_bootcamp_pytorch/chapter15_project_practice_1/trainer.py
@@ -23,70 +23,57 @@ class Trainer():
 
         return X, y
 
-    def _train(self, x, y, config):
+    def _train(self, X, y, config):
         self.model.train()
 
-        x, y = self._batchify(x, y, config.batch_size)
+        X, y = self._batchify(X, y, config.batch_size, random_split=True)
         total_loss = 0
 
-        for i, (x_i, y_i) in enumerate(zip(x, y)):
-            y_hat_i = self.model(x_i)
+        for i, (X_i, y_i) in enumerate(zip(X, y)):
+            y_hat_i = self.model(X_i)
             loss_i = self.crit(y_hat_i, y_i.squeeze())
 
-            # Initialize the gradients of the model.
             self.optimizer.zero_grad()
             loss_i.backward()
-
             self.optimizer.step()
 
             if config.verbose >= 2:
-                print("Train Iteration(%d/%d): loss=%.4e" % (i + 1, len(x), float(loss_i)))
+                print(f"Train Iteration {i + 1}/{len(X)}: loss={float(loss_i):.6f}")
 
-            # Don't forget to detach to prevent memory leak.
             total_loss += float(loss_i)
 
-        return total_loss / len(x)
+        return total_loss / len(X)
 
-    def _validate(self, x, y, config):
-        # Turn evaluation mode on.
+    def _validate(self, X, y, config):
         self.model.eval()
 
-        # Turn on the no_grad mode to make more efficintly.
         with torch.no_grad():
-            x, y = self._batchify(x, y, config.batch_size, random_split=False)
+            X, y = self._batchify(X, y, config.batch_size, random_split=False)
             total_loss = 0
 
-            for i, (x_i, y_i) in enumerate(zip(x, y)):
-                y_hat_i = self.model(x_i)
+            for i, (X_i, y_i) in enumerate(zip(X, y)):
+                y_hat_i = self.model(X_i)
                 loss_i = self.crit(y_hat_i, y_i.squeeze())
 
                 if config.verbose >= 2:
-                    print("Valid Iteration(%d/%d): loss=%.4e" % (i + 1, len(x), float(loss_i)))
+                    print(f"Valid Iteration {i + 1}/{len(X)}: loss={float(loss_i):.6f}")
 
                 total_loss += float(loss_i)
 
-            return total_loss / len(x)
+            return total_loss / len(X)
 
     def train(self, train_data, valid_data, config):
         lowest_loss = np.inf
         best_model = None
 
-        for epoch_index in range(config.n_epochs):
+        for epoch_i in range(config.n_epochs):
             train_loss = self._train(train_data[0], train_data[1], config)
             valid_loss = self._validate(valid_data[0], valid_data[1], config)
 
-            # You must use deep copy to take a snapshot of current best weights.
             if valid_loss <= lowest_loss:
                 lowest_loss = valid_loss
                 best_model = deepcopy(self.model.state_dict())
 
-            print("Epoch(%d/%d): train_loss=%.4e  valid_loss=%.4e  lowest_loss=%.4e" % (
-                epoch_index + 1,
-                config.n_epochs,
-                train_loss,
-                valid_loss,
-                lowest_loss,
-            ))
+            print(f"Epoch {epoch_i + 1}/{config.n_epochs}: train_loss={train_loss:.6f}  valid_loss={valid_loss:.6f}  lowest_loss={lowest_loss:.6f}")
 
-        # Restore to best model.
         self.model.load_state_dict(best_model)
