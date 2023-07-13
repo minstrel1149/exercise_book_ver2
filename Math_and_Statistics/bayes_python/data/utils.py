97415cc4f22ff657dd153d17ff798873e92b73d2
Taehwa Kim <kth1149@gmail.com>
2023-07-13 15:51:36 +0900

    230713

--- Math_and_Statistics/bayes_python/data/utils.py
+++ Math_and_Statistics/bayes_python/data/utils.py
@@ -4,3 +4,8 @@ def prob(ser):
 def conditional(proposition, given):
     return prob(proposition[given])
 
+def update(table):
+    table['unnorm'] = table['prior'] * table['likelihood']
+    table['posterior'] = table['unnorm'] / table['unnorm'].sum()
+    return table
+
